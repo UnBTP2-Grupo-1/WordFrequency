10848a7d20cea0fa0d5ce6404666254d2dd8e4dc
Gustavo <gustavopchaves@hotmail.com>
2021-05-08 17:19:39 -0300

    Removed unecessary comments

--- src/aux_functions.cpp
+++ src/aux_functions.cpp
@@ -1,31 +1,6 @@
 #include "../header/aux_functions.hpp"
 #include <fstream>
 
-// int main() {
-//     std::ifstream in;
-//     in.open("input_file.txt")
-//     // Fixed size array used to store the elements in the text file.
-//     // Change array type according to the type of the elements you want to read from the file
-//     int v[5];
-//     int element;
-//     if (in.is_open()) {
-//         int i = 0;
-//         while (in >> element) {
-//             v[i++] = element;
-//         }
-//     }
-//     in.close();
-//     return 0;
-// }
-
-// vector<string> word_count(string path) {
-//   vector<string> result;
-
-//   cout << path;
-
-//   return result;
-// }
-
 vector<string> extract_words(string path){
   vector<string> word_list;
   ifstream input;
